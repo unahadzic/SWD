36f58d06e93d937fc9356a451b9cfc7f65b860c0
Patrick Kainer <kainer.patrick@gmail.com>
2022-05-20 14:00:58 +0200

    added Groundobjekt.java

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/groundObject/Groundobjekt.java
@@ -0,0 +1,8 @@
+package at.compus02.swd.ss2022.game.groundObject;
+
+import at.compus02.swd.ss2022.game.gameobjects.GameObject;
+
+public interface Groundobjekt extends GameObject {
+
+    public int getSize();
+}
